a2b08ca1668793c0019ba704276667c1a50cf7e1
Exsnipe <exsnipe@yandex.ru>
2021-10-27 13:39:26 -0700

    class Win

--- puzzle/src/main/java/ru/job4j/puzzle/Win.java
+++ puzzle/src/main/java/ru/job4j/puzzle/Win.java
@@ -2,7 +2,34 @@ package ru.job4j.puzzle;
 
 public class Win {
     public static boolean check(int[][] board) {
-        boolean rsl = true;
-        return rsl;
+        return isHorizontalWin(board) || isVerticalWin(board);
+    }
+
+    public static boolean isHorizontalWin(int[][] board) {
+        for (int i = 0; i < board.length; i++) {
+            if (board[i][i] == 1) {
+                for (int j = 0; j < board.length; j++) {
+                    if (board[i][j] != 1) {
+                        return false;
+                    }
+                }
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean isVerticalWin(int[][] board) {
+        for (int i = 0; i < board.length; i++) {
+            if (board[i][i] == 1) {
+                for (int j = 0; j < board.length; j++) {
+                    if (board[j][i] != 1) {
+                        return false;
+                    }
+                }
+                return true;
+            }
+        }
+        return false;
     }
 }
